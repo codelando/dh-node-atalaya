0e9a1d6885b4b3c67dcd9287e8ae6e98f94e8a8c
Ezequiel Cortés <codelando@gmail.com>
2020-06-30 21:48:13 -0300

    HTML + CSS - Clase 1 - HTTP y Express

--- m4-html-css/c1-node-express/repaso/express.js
+++ m4-html-css/c1-node-express/repaso/express.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 
+// Podemos enviar HTML
 app.get('/', function (req, res) {
     res.send('<h1>¡Página principal!</h1>')
 });
@@ -14,8 +15,12 @@ const dependencies = {
     }
 }
 
+// También podemos enviar cualquier tipo de dato de JS
+// Express se encargará de convertirlo a JSON
 app.get('/package', function (req, res) {
     res.send(dependencies);
 })
 
+
+// Escuchamos en el puerto 3000
 app.listen(3000);
