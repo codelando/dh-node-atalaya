8bf6a4a4333e5899f83c34d8310cefce51ab2341
Ezequiel Cortés <codelando@gmail.com>
2020-09-17 20:39:42 -0300

    JS Front - Clase 3 - Formularios, location, history

--- m7-js-front/c3-formularios/repaso/js/validations.js
+++ m7-js-front/c3-formularios/repaso/js/validations.js
@@ -1,35 +1,46 @@
-// createForm: [
+window.addEventListener('load', function() {
+    
+    let errors = {};
+
+    // Capturamos los elementos
+    let form = document.getElementById('groupCreateForm');
+    let name = document.getElementById('name');
+
+    let validateName = function() {
+        let feedback = '';
         
-//     check('name')
-//         .notEmpty().withMessage('Debes completar el campo de nombre').bail()
-//         .isLength({ min: 5 }).withMessage('El nombre debe tener al menos 5 caracteres'), 
+        // Name es obligatorio y tiene que tener
+        if (validator.isEmpty(name.value, { ignore_whitespace:true })) {
+            feedback = 'El nombre no puede estar vacío';
+        } else if (!validator.isLength(name.value, { min: 5 })) {
+            feedback = 'El nombre debe tener al menos 5 caracteres';
+        }
+        
+        let feedbackElement = name.parentElement.nextElementSibling;
 
-//     check('categoryId')
-//         .notEmpty().withMessage('Debes elejir una categoría').bail(),
+        if (feedback) {
+            name.classList.add('is-danger');
+            feedbackElement.classList.add('is-danger');
+            errors.name = feedback;
+        } else {
+            name.classList.remove('is-danger');
+            feedbackElement.classList.remove('is-danger');
+            delete errors.name;
+        }
+        
+        name.parentElement.nextElementSibling.innerText = feedback;
+    };
 
-//     check('repository')
-//         .notEmpty().withMessage('Debes completar el campo de repositorio').bail()
-//         // https://express-validator.github.io/docs/custom-validators-sanitizers.html
-//         .custom(value => value.startsWith('https://github.com/')).withMessage('Debe ser una url de GitHub'),
-   
-//     check('description')
-//         .notEmpty().withMessage('Debes completar el campo de descripción').bail()
-//         .isLength({ min: 50 }).withMessage('La descripción debe tener al menos 50 caracteres'),
-// ],
+    name.addEventListener('blur', validateName);
 
+    form.addEventListener('submit', function (event) {
 
+        validateName();
 
-
-// on load
-
-// identificamos el form
-
-// identificamos los inputs
-
-// creamos nuestras funciones de validación
-
-// agregamos los eventos a cada input
-
-// agregamos el evento al form
-
-// si todo va bien dejamos que se envíe
+        // Si hay errores no enviamos el formulario
+        // https://stackoverflow.com/questions/126100/how-to-efficiently-count-the-number-of-keys-properties-of-an-object-in-javascrip
+        if (Object.keys(errors).length) {
+            event.preventDefault();
+        }
+    });
+});
